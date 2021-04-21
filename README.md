33b3164f09a946e146be750c3f14b6ca645b2bae
T-O-R-U-S <bageliq@protonmail.com>
2021-04-21 06:52:31 +0400

    Added more tests

---
+++ README.md
@@ -0,0 +1,28 @@
+# Threadder
+A simple multithreading implementation.
+Examples:
+
+```rust
+use std::{
+    thread::sleep,
+    time::Duration
+}
+
+fn main() {
+    // Warning: Threadpools MUST be declared as mutable!
+    // Create a 4-thread threadpool
+    let mut my_threadpool = ThreadPool::new(4);
+    // Due to a limiation of Rust, all clojures sent must be boxed to have a 'size known at compile time'... :'(
+    for _ in 0..4 {
+        my_threadpool.send(
+            Box::new(|| {
+                println!("Goodnight, world!");
+                sleep(Duration::from_secs(3));
+                println!("Goodmorning, world!");
+            })
+        );
+    }
+    // MUST be ran at the end of the program. Ensures all threads exit PROPERLY. Without this, all threads will shut down as soon as the 'main' thread shuts down -- which isn't desirable when your thread finishes 3 seconds AFTER main does.
+    my_threadpool.stop();
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use std::{
     panic::panic_any,
     sync::mpsc::{channel, Sender, TryRecvError},
-    thread::{spawn, JoinHandle},
+    thread::{spawn, JoinHandle, sleep},
+    time::Duration
 };
 
 pub struct Job(JoinHandle<()>, Sender<Box<dyn FnOnce() + Send>>);
@@ -77,6 +78,25 @@ fn send_thread() {
         println!("Hello world!!!!!");
     }));
     thread_pool.stop();
+}
+#[test]
+// Tests if thread exits properly
+fn test_safe_exit() {
+    // Warning: Threadpools MUST be declared as mutable!
+    // Create a 4-thread threadpool
+    let mut my_threadpool = ThreadPool::new(4);
+    // Due to a limiation of Rust, all clojures sent must be boxed to have a 'size known at compile time'... :'(
+    for _ in 0..4 {
+        my_threadpool.send(
+            Box::new(|| {
+                println!("Goodnight, world!");
+                sleep(Duration::from_secs(3));
+                println!("Goodmorning, world!");
+            })
+        );
+    }
+    // MUST be ran at the end of the program. Ensures all threads exit PROPERLY. Without this, all threads will shut down as soon as the 'main' thread shuts down -- which isn't desirable when your thread finishes 3 seconds AFTER main does.
+    my_threadpool.stop();
 }
 /*
 fn threadpool(jobs: Vec<Job>) {
